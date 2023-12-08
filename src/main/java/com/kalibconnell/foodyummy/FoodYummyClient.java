24eb8cbb325e8d4e1b59e34c8f2e31e4110a4e1e
Kalib Connell <bakermarilyn25@outlook.com>
2023-12-08 15:10:32 -0500

    Initial commit

---
+++ src/main/java/com/kalibconnell/foodyummy/FoodYummyClient.java
@@ -0,0 +1,11 @@
+package com.kalibconnell.foodyummy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FoodYummyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
